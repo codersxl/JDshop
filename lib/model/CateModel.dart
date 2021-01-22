6e7b13e724bd23cd7af3317d8e959ba6bb6dc679
xl <414920289@qq.com>
2021-01-22 16:38:11 +0800

    “分类页面的数据渲染完成”

---
+++ lib/model/CateModel.dart
@@ -0,0 +1,53 @@
+class CateModel {
+  List<CateModelItem> result;
+
+  CateModel({this.result});
+
+  CateModel.fromJson(Map<String, dynamic> json) {
+    if (json['result'] != null) {
+      result = new List<CateModelItem>();
+      json['result'].forEach((v) {
+        result.add(new CateModelItem.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.result != null) {
+      data['result'] = this.result.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class CateModelItem {
+  String sId;
+  String title;
+  Object status;
+  String pic;
+  String pid;
+  String sort;
+
+  CateModelItem({this.sId, this.title, this.status, this.pic, this.pid, this.sort});
+
+  CateModelItem.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    title = json['title'];
+    status = json['status'];
+    pic = json['pic'];
+    pid = json['pid'];
+    sort = json['sort'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['title'] = this.title;
+    data['status'] = this.status;
+    data['pic'] = this.pic;
+    data['pid'] = this.pid;
+    data['sort'] = this.sort;
+    return data;
+  }
+}
